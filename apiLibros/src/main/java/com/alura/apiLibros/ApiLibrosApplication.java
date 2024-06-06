7d3d687eb88baa8bcf922c88bd0d0bcf27bcbb01
juan-contrerasM <115199779+juan-contrerasM@users.noreply.github.com>
2024-06-06 09:43:29 -0500

    creacion proyecto

---
+++ apiLibros/src/main/java/com/alura/apiLibros/ApiLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.alura.apiLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibrosApplication.class, args);
+	}
+
+}
